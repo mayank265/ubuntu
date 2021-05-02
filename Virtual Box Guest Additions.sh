da9c56a408f83bb808bff215c10ca745b0217711
Mayank Agarwal <mayank265@gmail.com>
2021-05-02 14:44:49 +0530

    Create Virtual Box Guest Additions.sh

---
+++ Virtual Box Guest Additions.sh
@@ -0,0 +1,11 @@
+So basically:
+
+Open your terminal
+sudo apt-get install build-essential gcc make perl dkms
+reboot
+Open your terminal
+Go to the installation disk
+sudo sh VBoxLinuxAdditions.run
+reboot
+
+https://askubuntu.com/questions/1035030/virtualbox-guest-additions-installation-problem
